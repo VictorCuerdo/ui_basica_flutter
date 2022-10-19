217cf6207754f6b51eeeac7f7a16ff83683a70fd
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2022-10-18 21:35:05 -0500

    BotonDireccionyBusqueda
    Se establecen los margenes para los botones de la vista principal

--- lib/home/main_tiendas_page.dart
+++ lib/home/main_tiendas_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/colors.dart';
+
 class MainTiendasPage extends StatefulWidget {
   const MainTiendasPage({Key? key}) : super(key: key);
 
@@ -11,21 +13,32 @@ class _MainTiendasPageState extends State<MainTiendasPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            child: Row(
-      children: [
-        Column(
-          children: [Text("Ciudad"), Text("Dirección")],
-        ),
-        Container(
-          width: 45,
-          height: 45,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.blue,
-          ),
-        )
-      ],
-    )));
+      body: Column(
+        children: [
+          Container(
+              child: Container(
+                  margin: EdgeInsets.only(top: 45, bottom: 15),
+                  padding: EdgeInsets.only(left: 100, right: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [Text("Ciudad"), Text("Dirección")],
+                      ),
+                      Center(
+                          child: Container(
+                        width: 200,
+                        height: 45,
+                        child: Icon(Icons.search, color: Colors.white),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: AppColors.mainColor,
+                        ),
+                      ))
+                    ],
+                  )))
+        ],
+      ),
+    );
   }
 }
