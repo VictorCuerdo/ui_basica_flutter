b41e634b215f876d6c4ca180f038648b05be1385
Victor Cuerdo <vcuerdo@uninorte.edu.co>
2022-10-20 19:37:09 -0500

    sizingColumnBoxes

--- lib/home/productos_page_body.dart
+++ lib/home/productos_page_body.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+import '../widgets/texto_grande.dart';
 
 class ProductosPageBody extends StatefulWidget {
   const ProductosPageBody({super.key});
@@ -8,11 +12,13 @@ class ProductosPageBody extends StatefulWidget {
 }
 
 class _ProductosPageBodyState extends State<ProductosPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.65);
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -21,13 +27,55 @@ class _ProductosPageBodyState extends State<ProductosPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-        height: 220,
-        margin: EdgeInsets.only(left: 5, right: 5),
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-            image: DecorationImage(
-                image: AssetImage("assets/images/leche01.jpeg"))));
+    return Stack(
+      children: [
+        Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/manzana01.jpg")))),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 20, right: 20, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+                padding: EdgeInsets.only(top: 15, left: 15, right: 15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextoGrande(text: "Manzanas"),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      children: [
+                        Wrap(
+                            children: List.generate(
+                                5,
+                                (index) => Icon(
+                                      Icons.star,
+                                      color: AppColors.mainColor,
+                                      size: 15,
+                                    ))),
+                        SizedBox(
+                          width: 10,
+                        ),
+                      ],
+                    )
+                  ],
+                )),
+          ),
+        )
+      ],
+    );
   }
 }
